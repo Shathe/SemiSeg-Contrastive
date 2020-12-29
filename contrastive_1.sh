9e1a20e8f7a4aaf5dbe0ea8a1e979520bfae6a47
Inigo <inigol22zgz@gmail.com>
2020-12-29 23:31:27 +0100

    correction

--- contrastive_1.sh
+++ contrastive_1.sh
@@ -1,5 +1,5 @@
 echo "NEW FILTERING Wise"
-python3 contrastive_wise_manual_filter.py --config ./configs/configSSL_city_1_30_split0.json --name SSL --gpus 1
+python3 contrastive_wise_manual_filer.py --config ./configs/configSSL_city_1_30_split0.json --name SSL --gpus 1
 echo "Contrastive Random"
 python3 contrastive_random.py --config ./configs/configSSL_city_1_30_split0.json --name SSL --gpus 1
 
