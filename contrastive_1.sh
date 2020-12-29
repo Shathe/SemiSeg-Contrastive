ba54150223324c3b05e9f80e44f71e8160df0eeb
Inigo <inigol22zgz@gmail.com>
2020-12-29 16:33:19 +0100

    change scripts

--- contrastive_1.sh
+++ contrastive_1.sh
@@ -1,5 +1,3 @@
-echo "Contrastive high res, WISE"
-python3 contrastive_wise_manual_fullres.py --config ./configs/configSSL_city_1_30_split0.json --name SSL --gpus 1
 echo "Contrastive Random"
 python3 contrastive_random.py --config ./configs/configSSL_city_1_30_split0.json --name SSL --gpus 1
 
