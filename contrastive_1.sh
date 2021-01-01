6d03525f31dab2df17bea06467244094384ea82e
Inigo <inigol22zgz@gmail.com>
2021-01-01 12:48:32 +0000

    output results

--- contrastive_1.sh
+++ contrastive_1.sh
@@ -1,5 +1,2 @@
 echo "NEW FILTERING Wise"
 python3 contrastive_wise_manual_filter.py --config ./configs/configSSL_city_1_30_split0.json --name SSL --gpus 1
-echo "Contrastive Random"
-python3 contrastive_random.py --config ./configs/configSSL_city_1_30_split0.json --name SSL --gpus 1
-
