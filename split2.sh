50fb502663b06b0eba920a07cd5ffab6999ab0dd
Inigo <inigol22zgz@gmail.com>
2021-01-03 17:33:32 +0100

    last config

--- split2.sh
+++ split2.sh
@@ -1,5 +1,5 @@
 echo "deeplabv2 split 2"
-python3 trainSSL.py --config ./configs/configSSL_city_1_30_split2.json --name SSL --gpus 1
+python3 trainSSL3.py --config ./configs/configSSL_city_1_30_split2.json --name SSL --gpus 1
 
 echo "deeplabv3+ split 2"
-python3 trainSSL_fullres.py --config ./configs/configSSL_city_1_30_split2_v3.json --name SSL --gpus 1
+python3 trainSSL_fullres3.py --config ./configs/configSSL_city_1_30_split2_v3.json --name SSL --gpus 1
